f9716dc18b442357405b283a9c45153accce93b1
meraj arshad <merajsap184@gmail.com>
2024-01-20 12:26:19 +0530

    modify some extra file

--- script.js
+++
@@ -1,9 +0,0 @@
-  // document.getElementById('signup').addEventListener('click',signupfunc);
-
-  // function signupfunc(){
-  //   window.location.href='./signuppage/index.html'
-  // }
-
-  // document.querySelector("#login").addEventListener("click" , ()=>{
-  //   window.location.href='./loginpage/index.html'
-  // })
